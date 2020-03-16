76e0c15898667ff5c05bfae38f7431cc6e0cff7c
mwestt <m.west2718@gmail.com>
2020-03-16 16:41:40 -0400

    clean up code

--- Project/EDA.py
+++ Project/EDA.py
@@ -3,10 +3,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os 
+
 from sklearn.model_selection import cross_val_score
 from xgboost import XGBClassifier, plot_importance
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
+
 # Open dataframe
 df = pd.read_csv("data.csv")
 
@@ -16,8 +18,9 @@ y = df["diagnosis"]
 df = df.drop(columns=['Unnamed: 32', 'id', 'diagnosis'])
 
 model = LogisticRegression()
-# model.fit(df, y)
+model.fit(df, y)
 scores = cross_val_score(model, df, y, cv=5)
+
 print(scores, np.mean(scores), np.std(scores))
-# plot_importance(model)
-# plt.show()
+plot_importance(model)
+plt.show()
